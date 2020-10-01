3de9e17f137090fa45ec10fb196fb2dc2225c158
uzzu <himeatball@gmail.com>
2020-10-01 18:03:45 +0900

    Update kotlin to 1.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
     id("co.uzzu.dotenv.gradle") version "1.1.0"
-    kotlin("jvm") version "1.4.0" apply false
+    kotlin("jvm") version "1.4.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
     id("org.jetbrains.dokka") version "1.4.0" apply false
 }
